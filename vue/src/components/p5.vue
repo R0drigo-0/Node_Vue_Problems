c8ba4e8313f58429878027494818c1f85830242b
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2025-06-02 23:30:09 +0200

    P5

--- vue/src/components/p5.vue
+++ vue/src/components/p5.vue
@@ -1,7 +1,25 @@
 ﻿<script>
+export default {
+  name: "P5",
+  data() {
+    return {
+      buffer: ''
+    };
+  },
+  watch: {
+    buffer(val) {
+      if (val.length >= 5) {
+        this.buffer = '';
+      }
+    }
+  }
+}
 </script>
 
 <template>
+  <form>
+    <input type="text" v-model="buffer" />
+  </form>
 </template>
 
 <style scoped>
