d7206a9e030323bc55a6267081ef2d48e81eef37
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2025-06-02 23:17:22 +0200

    P4

--- vue/src/components/p4.vue
+++ vue/src/components/p4.vue
@@ -1,8 +1,29 @@
 ﻿<script>
+export default {
+  name: "P3",
+  data() {
+    return {
+      active: true
+    };
+  },
+  methods: {
+    toggle() {
+      this.active = !this.active;
+    }
+  },
+};
 </script>
 
 <template>
+  <button v-on:click="toggle" v-bind:disabled="!active">Click me!</button>
 </template>
 
 <style scoped>
+button {
+  border: 1px solid #cecece;
+  border-radius: 5px;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+}
 </style>
