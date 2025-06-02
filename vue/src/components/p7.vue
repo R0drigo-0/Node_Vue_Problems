66a6239e780e1e3e607d331280ca39628bfb0ca0
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2025-06-02 23:39:08 +0200

    P7

--- vue/src/components/p7.vue
+++ vue/src/components/p7.vue
@@ -1,8 +1,41 @@
 ﻿<script>
+export default {
+  name: "P6",
+  data() {
+    return {
+      buffer: '',
+      isRedBg: false,
+    };
+  },
+  methods: {
+    setRedBg() {
+      this.isRedBg = true;
+    },
+    unsetRedBg() {
+      this.isRedBg = false;
+    },
+  },
+}
 </script>
 
 <template>
+  <form>
+    <input
+      type="text"
+      v-model="buffer"
+      :class="{ pressed: isRedBg, released: !isRedBg }"
+      v-on:keydown="setRedBg"
+      v-on:keyup="unsetRedBg"
+    />
+  </form>
 </template>
 
 <style scoped>
+input.pressed{
+background-color: red;
+}
+input.released{
+background-color: aqua;
+}
+
 </style>
