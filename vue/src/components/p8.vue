eac3fa651c45251237482ae41e32767bc135a4c9
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2025-06-02 23:44:23 +0200

    P8

--- vue/src/components/p8.vue
+++ vue/src/components/p8.vue
@@ -1,8 +1,20 @@
 ﻿<script>
+export default {
+  name: "P8",
+  data() {
+    return {
+      x: 0
+    };
+  },
+};
 </script>
 
 <template>
+<div>
+  <div v-bind:style="{ color: `hsl(0,${x}%,50%)` }">AM I RED?</div>
+    <input v-model="x" type="range" min="0" max="100">
+  <div>YES!</div>
+</div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
