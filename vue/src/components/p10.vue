3a82640ec2428bd97397b29c2fac0fc25d3be0dd
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2025-06-03 00:37:09 +0200

    9 & 10

--- vue/src/components/p10.vue
+++ vue/src/components/p10.vue
@@ -1,8 +1,50 @@
 ﻿<script>
+export default {
+  name: "P10",
+  data() {
+    return {
+      root: false
+    };
+  },
+  computed: {
+    a: {
+      get() {
+        return this.root;
+      },
+      set(val) {
+        this.root = val;
+      }
+    },
+    b() {
+      return this.a;
+    },
+    c() {
+      return this.a;
+    },
+    d() {
+      return this.a;
+    }
+  }
+};
 </script>
 
 <template>
+  <div>
+    <input type="checkbox" v-model="a" id="inputCheckbox2"/>
+    <label for="inputCheckbox2">{{ a }}</label>
+    <label for="inputCheckbox2">{{ b }}</label>
+    <label for="inputCheckbox2">{{ c }}</label>
+    <label for="inputCheckbox2">{{ d }}</label>
+  </div>
 </template>
 
 <style scoped>
+div {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+div > label {
+  margin-right: 5px;
+}
 </style>
--- vue/src/components/p9.vue
+++ vue/src/components/p9.vue
@@ -1,8 +1,45 @@
 ﻿<script>
+export default {
+  name: "P9",
+  data() {
+    return {
+      a: false,
+      b: false,
+      c: false,
+      d: false,
+    };
+  },
+  watch: {
+    a(newValue) {
+      this.b = newValue;
+    },
+    b(newValue) {
+      this.c = newValue;
+    },
+    c(newValue) {
+      this.d = newValue;
+    },
+  },
+};
 </script>
 
 <template>
+  <div>
+    <input type="checkbox" v-model="a" id="inputCheckbox"/>
+    <label for="inputCheckbox"> {{ a }}</label>
+    <label for="inputCheckbox"> {{ b }}</label>
+    <label for="inputCheckbox"> {{ c }}</label>
+    <label for="inputCheckbox"> {{ d }}</label>
+  </div>
 </template>
 
 <style scoped>
+div {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+div > label {
+  margin-right: 5px;
+}
 </style>
