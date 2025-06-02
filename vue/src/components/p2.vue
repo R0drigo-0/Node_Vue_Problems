356658ae0dc8ca3a35d29cb481aa86ec75859db9
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2025-06-02 23:06:20 +0200

    P2

--- vue/src/components/p2.vue
+++ vue/src/components/p2.vue
@@ -1,8 +1,32 @@
 ﻿<script>
+export default {
+  name: "P2",
+  data() {
+    return {
+      a: 0,
+      b:0,
+    }
+  },
+  computed: {
+    addition() {
+      return Number(this.a) + Number(this.b);
+    }
+  },
+}
 </script>
 
 <template>
+  <form>
+    <input v-model="a" type="number" />
+    <input v-model="b" type="number" />
+    <span>{{ this.addition }}</span>
+  </form>
 </template>
 
 <style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 </style>
