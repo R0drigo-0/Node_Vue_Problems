2fe40c38bea3e7a961ce8e0e9de80bf3276b713f
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2025-06-03 16:22:20 +0200

    P20

---
+++ vue/src/components/ColorSelector.vue
@@ -0,0 +1,74 @@
+<script>
+export default {
+  name: "ColorSelector",
+  emits: ["red", "green", "blue"],
+  data() {
+    return {
+      red: 0,
+      green: 0,
+      blue: 0,
+    };
+  },
+};
+</script>
+
+<template>
+  <div id="component">
+    <div
+      id="box"
+      v-bind:style="{ backgroundColor: `rgb(${red}, ${green}, ${blue})` }"
+    ></div>
+    <div id="inputs">
+      <div>
+        R:
+        <input
+          v-model="red"
+          type="range"
+          min="0"
+          max="255"
+          v-on:input="$emit('red', Number(red))"
+        />
+        red value
+      </div>
+      <div>
+        G:
+        <input
+          v-model="green"
+          type="range"
+          min="0"
+          max="255"
+          v-on:input="$emit('green', Number(green))"
+        />
+        green value
+      </div>
+      <div>
+        B:
+        <input
+          v-model="blue"
+          type="range"
+          min="0"
+          max="255"
+          v-on:input="$emit('blue', Number(blue))"
+        />
+        blue value
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#component {
+  border: solid;
+  display: flex;
+}
+#box {
+  background-color: #000;
+  width: 110px;
+  height: 110px;
+}
+#inputs {
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+}
+</style>
--- vue/src/components/p20.vue
+++ vue/src/components/p20.vue
@@ -1,8 +1,51 @@
 ﻿<script>
+import ColorSelector from "./ColorSelector.vue";
+export default {
+  name: "P20",
+  components: {
+    ColorSelector,
+  },
+  data() {
+    return {
+      red: 0,
+      green: 0,
+      blue: 0,
+    };
+  },
+};
 </script>
 
 <template>
+  <div id="main">
+    <!-- Here goes the child component -->
+    <ColorSelector
+      v-on:red="
+        (val) => {
+          red = val;
+        }
+      "
+      v-on:green="
+        (val) => {
+          green = val;
+        }
+      "
+      v-on:blue="
+        (val) => {
+          blue = val;
+        }
+      "
+    />
+    <div
+      v-bind:style="{color: `rgb(${red}, ${green}, ${blue})`}"
+    >
+      TEXT
+    </div>
+  </div>
 </template>
 
 <style scoped>
+#main {
+  border: solid red;
+  display: flex;
+}
 </style>
