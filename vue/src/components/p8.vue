5b7a26e6aa28496babd7bac4aabaf45eaf18b9a9
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2025-06-03 09:35:01 +0200

    Small Fix P8

--- vue/src/components/p8.vue
+++ vue/src/components/p8.vue
@@ -13,7 +13,8 @@ export default {
 <div>
   <div v-bind:style="{ color: `hsl(0,${x}%,50%)` }">AM I RED?</div>
     <input v-model="x" type="range" min="0" max="100">
-  <div>YES!</div>
+  <div v-if="x<33">NO!</div>
+  <div v-else>YES!</div>
 </div>
 </template>
 
