292d6ab6cd2e5394a53cdb8415ce2bf0b072d229
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2025-06-03 17:03:25 +0200

    DoubleCheckbox

--- vue/src/components/p21.vue
+++ vue/src/components/p21.vue
@@ -1,8 +1,23 @@
 ﻿<script>
+import DoubleCheckbox from "./DoubleCheckbox.vue";
+export default {
+  name: "P21",
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    DoubleCheckbox,
+  },
+};
 </script>
 
 <template>
+  <div>
+    <input type="checkbox" />
+    <DoubleCheckbox/>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
