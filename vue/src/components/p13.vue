91d70e7444dec925f3b164e680386dbad27049b8
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2025-06-03 10:35:01 +0200

    P13

--- vue/src/components/p13.vue
+++ vue/src/components/p13.vue
@@ -1,8 +1,62 @@
 ﻿<script>
+export default {
+  name: "P13",
+  data() {
+    return {
+      state: 0,
+    };
+  },
+  watch: {
+    state(newState) {
+      if (newState == 3) {
+        this.state = 0;
+      }
+    },
+  },
+  methods: {
+    handleClick() {
+      this.state++;
+    },
+  },
+};
 </script>
 
 <template>
+  <div class="trafficlight">
+    <div class="light" v-bind:class="state == 2 ? 'redOn' : 'redOff'"></div>
+    <div
+      class="light"
+      v-bind:class="state == 1 ? 'yellowOn' : 'yellowOff'"
+    ></div>
+    <div class="light" v-bind:class="state == 0 ? 'greenOn' : 'greenOff'"></div>
+    <button v-on:click="handleClick">Switch</button>
+  </div>
 </template>
 
 <style scoped>
+div.trafficlight {
+  display: inline-block;
+  width: 30px;
+}
+div.light {
+  height: 30px;
+}
+div.redOn {
+  background-color: red;
+}
+div.redOff {
+  background-color: indianred;
+}
+div.yellowOn {
+  background-color: yellow;
+}
+div.yellowOff {
+  background-color: khaki;
+}
+div.greenOn {
+  background-color: lime;
+}
+div.greenOff {
+  background-color: seagreen;
+}
 </style>
