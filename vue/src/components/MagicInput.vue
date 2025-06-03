63a71a53bc12f6d8021b21891257ea4356972e7f
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2025-06-03 18:21:02 +0200

    P22

---
+++ vue/src/components/MagicInput.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: "MagicInput",
+  props: {
+    modelValue: String,
+  },
+  emits: ["update:modelValue"],
+  computed: {
+    displayValue() {
+      if (!this.modelValue) {
+        return "";
+      }
+      return this.modelValue.replace(/./g, (x) =>
+        x.toUpperCase() == x ? x.toLowerCase() : x.toUpperCase()
+      );
+    },
+  },
+  methods: {
+    onInput(e) {
+      const switched = e.target.value.replace(/./g, (x) =>
+        x.toUpperCase() == x ? x.toLowerCase() : x.toUpperCase()
+      );
+      this.$emit("update:modelValue", switched);
+    },
+  },
+};
+</script>
+
+<template>
+  <input
+    type="text"
+    :value="displayValue"
+    @input="onInput"
+    placeholder="Magic Input"
+  />
+</template>
+
+<style scoped></style>
--- vue/src/components/p22.vue
+++ vue/src/components/p22.vue
@@ -1,8 +1,30 @@
 ﻿<script>
+import MagicInput from "./MagicInput.vue";
+export default {
+  name: "P22",
+  components: {
+    MagicInput,
+  },
+  data() {
+    return {
+      text: "",
+      buffer: "",
+    };
+  },
+};
 </script>
 
 <template>
+  <div>
+    <MagicInput v-model="buffer"/>
+    <input v-model="text" type="text" placeholder="Normal Input" />
+    <span v-if="buffer">Buffer: {{ this.buffer }}</span>
+  </div>
 </template>
 
 <style scoped>
+div {
+  display: flex;
+  flex-direction: row;
+}
 </style>
