bc5933125fb2620c5d8409e67e6c3f79f56243c9
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2025-06-03 11:34:46 +0200

    P15

---
+++ vue/src/components/WordsToList.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: "WordsToList",
+  props: {
+    words: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    wordList() {
+      return this.words.trim().split(' ')
+    },
+  },
+};
+</script>
+
+<template>
+  <ul>
+    <li v-for="(element, index) in wordList" v-bind:key="index">{{ element }}</li>
+  </ul>
+</template>
+
+<style scoped></style>
--- vue/src/components/p15.vue
+++ vue/src/components/p15.vue
@@ -1,7 +1,20 @@
 ﻿<script>
+import WordsToList from './WordsToList.vue';
+
+export default {
+  name: "P15",
+  components: {
+    WordsToList
+  },
+  data() {
+    return {
+    }
+  }
+}
 </script>
 
 <template>
+  <WordsToList words="Lorem ipsum dolor sit amet"/>
 </template>
 
 <style scoped>
