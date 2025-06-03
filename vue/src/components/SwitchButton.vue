bf6ba46a83d3bb3a76e336372905cc094c9d18ce
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2025-06-03 15:40:15 +0200

    P19

---
+++ vue/src/components/SwitchButton.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  name: "SwitchButton",
+  emits: ["on", "off"],
+  data() {
+    return {
+      isOnEnabled: true,
+      isOffEnabled: false,
+    };
+  },
+  methods: {
+    handleOn() {
+      this.isOnEnabled = !this.isOnEnabled;
+      this.isOffEnabled = !this.isOffEnabled;
+      this.$emit("on");
+    },
+    handleOff() {
+      this.isOnEnabled = !this.isOnEnabled;
+      this.isOffEnabled = !this.isOffEnabled;
+      this.$emit("off");
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <button v-on:click="handleOn" v-bind:disabled="isOnEnabled">ON</button>
+    <button v-on:click="handleOff" v-bind:disabled="isOffEnabled">OFF</button>
+  </div>
+</template>
+
+<style scoped>
+  div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    border: 3px solid black;
+    border-radius: 4px;
+  }
+  button {
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+  }
+  button:disabled {
+    background-color: #ccc;
+    cursor: not-allowed;
+  }
+</style>
--- vue/src/components/p19.vue
+++ vue/src/components/p19.vue
@@ -1,8 +1,44 @@
 ﻿<script>
+import SwitchButton from "./SwitchButton.vue";
+export default {
+  name: "P19",
+  components: {
+    SwitchButton,
+  },
+  data() {
+    return {
+      state: "on",
+    };
+  },
+};
 </script>
 
 <template>
+  <div>
+    <SwitchButton
+    v-on:on="
+      () => {
+        this.state = 'on';
+      }
+      "
+    v-on:off="
+      () => {
+        this.state = 'off';
+      }
+      "
+  />
+  <p>Just turned {{ state }}</p>
+</div>
 </template>
 
 <style scoped>
+  div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  p {
+    margin-left: 10px;
+    font-size: 20px;
+  }
 </style>
