39c416a8a551aa67ad3feaa10dac7814bf690cdb
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2025-06-04 16:03:03 +0200

    2023

--- exams/2023/src/components/BookingSlotComponent.vue
+++ exams/2023/src/components/BookingSlotComponent.vue
@@ -1,12 +1,83 @@
 <script>
-export default{
-  
-}
+export default {
+  name: "BookingSlotComponent",
+  props: {
+    bikeId: {
+      type: Number,
+      required: true,
+    },
+    slotId: {
+      type: Number,
+      required: true,
+    },
+    modelValue: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ["update:modelValue"],
+  data() {
+    return {
+      isClicked: false,
+      bookedStatus: undefined,
+      backgroundColor: "",
+    };
+  },
+  methods: {
+    handleClick() {
+      if (!this.isClicked && !this.modelValue) {
+        this.isClicked = true;
+        this.backgroundColor = "yellow";
+        this.bookBike();
+      }
+    },
+    bookBike() {
+      fetch(
+        `http://localhost:3001/book?bikeId=${this.bikeId}&slotId=${this.slotId}`
+      )
+        .then((res) => {
+          return res.text();
+        })
+        .then((res) => {
+          if (res === "booked") {
+            this.backgroundColor = "green";
+            this.$emit("update:modelValue", true);
+            this.bookedStatus = "booked";
+          } else if (res === "rejected") {
+            this.backgroundColor = "red";
+            this.bookedStatus = "rejected";
+          } else {
+            console.error("Unexpected response:", res);
+            this.bookedStatus = "error";
+          }
+        })
+        .catch((err) => {
+          this.backgroundColor = "red";
+          console.error("Error booking bike:", err);
+        });
+    },
+  },
+  watch: {
+    modelValue: {
+      immediate: true,
+      handler(newVal) {
+        if (newVal && !this.isClicked) {
+          this.backgroundColor = "gray";
+        }
+      },
+    },
+  },
+};
 </script>
 
 <template>
-
+  <div>
+    <span
+      v-on:click="handleClick"
+      v-bind:style="{ backgroundColor: backgroundColor }"
+      >Bike:{{ bikeId }} Slot:{{ slotId }}</span
+    >
+  </div>
 </template>
 
-<style>
-</style>
+<style></style>
--- exams/2023/src/exam.js
+++ exams/2023/src/exam.js
@@ -14,7 +14,15 @@ const bookBikeModule = function(){
     return {
       state: 0, //0: off  1: on
       startTimeOut: function(){
-        //TODO  
+        //TODO
+        this.state = 1;
+        setTimeout(() => {
+          if (this.state === 1 && bookingPromises[bikeId]) {
+            bookingPromises[bikeId].resolve('booked');
+            this.state = 0;
+            bookingPromises[bikeId] = null;
+          }
+        }, 5000);  
       }
     }
   }
@@ -23,8 +31,29 @@ const bookBikeModule = function(){
   let bikesTimeouts = [buildTimeOut(1), buildTimeOut(2), buildTimeOut(3), buildTimeOut(4), buildTimeOut(5)]
 
   //TODO
+  let bookingPromises = {};
 
+  const bookBike = function(bikeId, slotId) {
+    return new Promise((resolve, reject) => {
+      if (bikeId === slotId) {
+        return reject('rejected');
+      }
+      
+      if (bookingPromises[bikeId]) {
+        bookingPromises[bikeId].reject('rejected');
+      }
+      
+      bookingPromises[bikeId] = { resolve, reject };
+      
+      if (bikesTimeouts[bikeId-1].state === 0) {
+        bikesTimeouts[bikeId-1].startTimeOut();
+      }
+    });
+  };
 
+  return {
+    bookBike: bookBike
+  };
 }()
 
 app.get('/book', function(req, res) {
@@ -34,6 +63,17 @@ app.get('/book', function(req, res) {
       //res.write('booked');
       //res.write('rejected');
       //hint: promise.then((value)=>{}).catch((err)=>{}).finally(()=>res.end())
+      const bikeId = parseInt(req.query.bikeId);
+      const slotId = parseInt(req.query.slotId);
+
+      bookBikeModule.bookBike(bikeId, slotId)
+        .then((value) => {
+          res.write(value); // 'booked'
+        })
+        .catch((err) => {
+          res.write(err); // 'rejected'
+        })
+        .finally(() => res.end());
       
     } else {
         // hardcoded response for when bookBike function is not implemented
